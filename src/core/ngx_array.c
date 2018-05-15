35b7f316b8afe1b45f3b820d4f8c4a3eab3a4bbc
ali94s <ali_netmail@163.com>
2018-05-15 15:06:01 +0800

    分析nginx数组结构的设计

--- src/core/ngx_array.c
+++ src/core/ngx_array.c
@@ -8,17 +8,19 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 
-
+/*
+*	创建nginx数组，初始化一个nginx数组的管理结构
+*/
 ngx_array_t *
 ngx_array_create(ngx_pool_t *p, ngx_uint_t n, size_t size)
 {
     ngx_array_t *a;
-
+	//在内存池申请内存给这个数组变量
     a = ngx_palloc(p, sizeof(ngx_array_t));
     if (a == NULL) {
         return NULL;
     }
-
+	//进行初始化
     if (ngx_array_init(a, p, n, size) != NGX_OK) {
         return NULL;
     }
@@ -26,31 +28,35 @@ ngx_array_create(ngx_pool_t *p, ngx_uint_t n, size_t size)
     return a;
 }
 
-
+/*
+*	销毁nginx数组
+*/
 void
 ngx_array_destroy(ngx_array_t *a)
 {
     ngx_pool_t  *p;
 
     p = a->pool;
-
+	//首先回收数组元素的内存
     if ((u_char *) a->elts + a->size * a->nalloc == p->d.last) {
         p->d.last -= a->size * a->nalloc;
     }
-
+	//再回收数组管理结构的内存
     if ((u_char *) a + sizeof(ngx_array_t) == p->d.last) {
         p->d.last = (u_char *) a;
     }
 }
 
-
+/*
+*	添加一个数组元素
+*/
 void *
 ngx_array_push(ngx_array_t *a)
 {
     void        *elt, *new;
     size_t       size;
     ngx_pool_t  *p;
-
+	//判断数组是都已经满
     if (a->nelts == a->nalloc) {
 
         /* the array is full */
@@ -58,7 +64,10 @@ ngx_array_push(ngx_array_t *a)
         size = a->size * a->nalloc;
 
         p = a->pool;
-
+		/*
+		*	数组结束位置是d.last         就是pool的可用位置
+		*	并且pool剩余内存足够分配一个size的数组元素
+		*/
         if ((u_char *) a->elts + size == p->d.last
             && p->d.last + a->size <= p->d.end)
         {
@@ -66,31 +75,42 @@ ngx_array_push(ngx_array_t *a)
              * the array allocation is the last in the pool
              * and there is space for new allocation
              */
-
+			//数组扩容一个元素
             p->d.last += a->size;
             a->nalloc++;
 
-        } else {
+        } 
+		/*
+		*	pool内存不足分配一个元素，则new一个新的数组
+		*	大小为:size = a->size * a->nalloc;
+		*/	
+		else {
             /* allocate a new array */
-
+			/*
+			*	分配新内存的时候并没有把原来pool中的数组释放
+			*	nginx对pool中内存的释放原则是：
+			*	要么释放整个pool，要么不释放
+			*/
             new = ngx_palloc(p, 2 * size);
             if (new == NULL) {
                 return NULL;
             }
-
+			//将原来的数组元素拷贝过去
             ngx_memcpy(new, a->elts, size);
             a->elts = new;
             a->nalloc *= 2;
         }
     }
-
+	//elt是新数组未使用的第一个元素的地址
     elt = (u_char *) a->elts + a->size * a->nelts;
-    a->nelts++;
+    a->nelts++;		//未使用的元素下标++,因为只分配一个元素
 
     return elt;
 }
 
-
+/*
+*	多个元素的push，添加多个元素
+*/
 void *
 ngx_array_push_n(ngx_array_t *a, ngx_uint_t n)
 {
@@ -99,6 +119,7 @@ ngx_array_push_n(ngx_array_t *a, ngx_uint_t n)
     ngx_uint_t   nalloc;
     ngx_pool_t  *p;
 
+	//添加多个元素
     size = n * a->size;
 
     if (a->nelts + n > a->nalloc) {
@@ -115,12 +136,15 @@ ngx_array_push_n(ngx_array_t *a, ngx_uint_t n)
              * and there is space for new allocation
              */
 
-            p->d.last += size;
-            a->nalloc += n;
+            p->d.last += size;	//池子可用内存向后偏
+            a->nalloc += n;		//元素个数+n
 
         } else {
             /* allocate a new array */
-
+			/*
+			*	分配新数组个数需要在n和a->nalloc之间取较大值
+			*	使用三木运算符a > b ? a : b;
+			*/
             nalloc = 2 * ((n >= a->nalloc) ? n : a->nalloc);
 
             new = ngx_palloc(p, nalloc * a->size);
@@ -135,7 +159,7 @@ ngx_array_push_n(ngx_array_t *a, ngx_uint_t n)
     }
 
     elt = (u_char *) a->elts + a->size * a->nelts;
-    a->nelts += n;
+    a->nelts += n;		//增加n个元素
 
     return elt;
 }
--- src/core/ngx_array.h
+++ src/core/ngx_array.h
@@ -13,21 +13,42 @@
 #include <ngx_core.h>
 
 
+/*
+*	nginx数组的使用是依赖ngx内存池的
+*	并且nginx数组是设计用来存储小数据的
+*/
 typedef struct {
-    void        *elts;
-    ngx_uint_t   nelts;
-    size_t       size;
-    ngx_uint_t   nalloc;
-    ngx_pool_t  *pool;
+    void        *elts;		//数组的内存地址，首元素指针
+    ngx_uint_t   nelts;		//未使用元素的索引
+    size_t       size;		//每个元素的大小
+    ngx_uint_t   nalloc;	//分配元素个数
+    ngx_pool_t  *pool;		//内存池，就是整个内存池的首节点
 } ngx_array_t;
 
-
+/*
+*	创建nginx数组
+*	初始化了一个nginx数组的管理结构ngx_array_t*类型
+*/
 ngx_array_t *ngx_array_create(ngx_pool_t *p, ngx_uint_t n, size_t size);
+/*
+*	销毁一个nginx数组
+*	销毁任务包括：	1.nginx数组内存
+*				   2.nginx数组管理结构的内存
+*/
 void ngx_array_destroy(ngx_array_t *a);
+/*
+*	添加一个新数组元素的内存
+*/
 void *ngx_array_push(ngx_array_t *a);
+/*
+*	添加n个新数组元素的内存
+*/
 void *ngx_array_push_n(ngx_array_t *a, ngx_uint_t n);
 
 
+/*
+*	nginx数组的初始化
+*/
 static ngx_inline ngx_int_t
 ngx_array_init(ngx_array_t *array, ngx_pool_t *pool, ngx_uint_t n, size_t size)
 {
@@ -38,9 +59,10 @@ ngx_array_init(ngx_array_t *array, ngx_pool_t *pool, ngx_uint_t n, size_t size)
 
     array->nelts = 0;
     array->size = size;
-    array->nalloc = n;
+    array->nalloc = n;		//元素个数
     array->pool = pool;
-
+	
+	//elts指向数组的内存地址
     array->elts = ngx_palloc(pool, n * size);
     if (array->elts == NULL) {
         return NGX_ERROR;
